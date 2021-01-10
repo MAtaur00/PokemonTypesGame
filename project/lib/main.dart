d213df14b947c5743252050d3a33bb46c0c3b239
MAtaur00 <mtmtarres@gmail.com>
2021-01-10 21:05:33 +0100

    new routes added

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -6,6 +6,9 @@ import 'screens/typeListScreen.dart';
 import 'screens/mainScreen.dart';
 import 'playerSettings.dart';
 import 'pokemonType.dart';
+import 'screens/current_lobbies.dart';
+import 'screens/lobby_pre_game.dart';
+import 'screens/scene_game.dart';
 
 //void main() => runApp(PokemonTypeApp());
 
@@ -22,6 +25,9 @@ class App extends StatelessWidget {
       routes: {
         '/': (context) => MainScreen(), //Main menu
         '/TLS': (context) => TypeListScreen(),
+        '/CL': (context) => CurrentLobbies(),
+        '/LS': (context) => LobbyScreen(),
+        '/SG': (context) => SceneGame(),
       },
     );
   }
